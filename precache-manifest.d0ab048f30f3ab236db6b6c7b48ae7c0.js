636ccc5a1a561405181545c6a9d0ef9ad2f6bd89
atiloladave <atiloladavid1@gmail.com>
2020-06-21 18:36:16 -1200

    Updates

---
+++ precache-manifest.d0ab048f30f3ab236db6b6c7b48ae7c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d87e4f9d8217e31a1138639be7c6634",
+    "url": "/mapapireact/index.html"
+  },
+  {
+    "revision": "48c2e4363615bf552a9f",
+    "url": "/mapapireact/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "ad04d8d4fcaaf01e7b4c",
+    "url": "/mapapireact/static/css/main.e1a3585b.chunk.css"
+  },
+  {
+    "revision": "48c2e4363615bf552a9f",
+    "url": "/mapapireact/static/js/2.d2609dff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mapapireact/static/js/2.d2609dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad04d8d4fcaaf01e7b4c",
+    "url": "/mapapireact/static/js/main.14758e71.chunk.js"
+  },
+  {
+    "revision": "342955dc30cda799ff4c",
+    "url": "/mapapireact/static/js/runtime-main.035b2f7f.js"
+  }
+]);
